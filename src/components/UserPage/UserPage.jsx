8078c58ae3b0e9649121147d6c7bc48b625c89e0
Alex Suedkamp <ajsuedkamp@gmail.com>
2022-10-23 18:15:43 -0500

    when click log in golf history is displayed

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
+import GolfHistory from '../GolfHistory/GolfHistory';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   return (
     <div className="container">
-      <h2>FORE!!!, {user.username}!</h2>
+      <h2>Next on the tee, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
+      <GolfHistory />
+      <br></br>
       <LogOutButton className="btn" />
     </div>
   );
